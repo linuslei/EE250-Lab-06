c13513871c5414481bdd1d6719c0cf7ccd5199a2
linuslei <linuslei@usc.edu>
2023-03-02 17:32:20 -0800

    1

--- grove_sensors.py
+++ grove_sensors.py
@@ -30,12 +30,10 @@ while True:
     measured_range = grovepi.ultrasonicRead(ultrasound)
 
     # Print to Screen
-    old_measured_range = measured_range
-    old_sensor_value = sensor_value
-
-    if (old_measured_range != measured_range) | (old_sensor_value != sensor_value):
-        if measured_range > range_ref:
-            setText("Threshold: " + str(range_ref)+ "\n Range: " + str(measured_range) + "cm")
-        else:
-            setText("Threshold: " + str(range_ref)+ "cm OBJ PRES\n Range: " + str(measured_range) + "cm")
     
+    if measured_range > range_ref:
+        setText("Threshold: " + str(range_ref)+ "\n Range: " + str(measured_range) + "cm")
+    else:
+        setText("Threshold: " + str(range_ref)+ "cm OBJ PRES\n Range: " + str(measured_range) + "cm")
+    
+    time.sleep(.1)
