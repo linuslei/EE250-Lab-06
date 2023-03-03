2ff50e1bcc54851623a6eb4900e7c26981d02a40
linuslei <linuslei@usc.edu>
2023-03-02 17:44:03 -0800

    final

--- grove_sensors.py
+++ grove_sensors.py
@@ -35,8 +35,8 @@ while True:
     # Print to Screen
     if (measured_range != old_measured_range) or (range_ref != old_range):
         if measured_range > range_ref:
-            setText("Threshold: " + str(range_ref)+ "\n Range: " + str(measured_range) + "cm")
+            setText(str(range_ref)+ "cm\n Range: " + str(measured_range) + "cm")
         else:
-            setText("Threshold: " + str(range_ref)+ "cm OBJ PRES\n Range: " + str(measured_range) + "cm")
+            setText(str(range_ref)+ "cm OBJ PRES\n Range: " + str(measured_range) + "cm")
     old_range = range_ref
     old_measured_range = measured_range
